ce12e05a81ff13cf0ae07e2b6af37cdc4d7e13de
AchmadPutraA12 <ahmad.arifki11@gmail.com>
2024-08-08 00:41:09 +0700

    kurang create produk

--- resources/js/Pages/Admin/Backup/Index.jsx
+++ resources/js/Pages/Admin/Backup/Index.jsx
@@ -102,26 +102,22 @@ const Index = ({ kategori, produk }) => {
         {
             id: "kategori.name",
             accessorKey: "kategori.name",
-            header: ({ column }) => {
-                return (
-                    <Button
-                        variant="tableHeader"
-                        onClick={() =>
-                            column.toggleSorting(column.getIsSorted() === "asc")
-                        }
-                    >
-                        Kategori
-                        <ArrowUpDown className="h-4 w-4" />
-                    </Button>
-                );
-            },
-            cell: ({ row }) => {
-                return (
-                    <span className=" bg-yellow-200 rounded-lg px-4 py-1">
-                        {row.original.kategori.name}
-                    </span>
-                );
-            },
+            header: ({ column }) => (
+                <Button
+                    variant="tableHeader"
+                    onClick={() =>
+                        column.toggleSorting(column.getIsSorted() === "asc")
+                    }
+                >
+                    Kategori
+                    <ArrowUpDown className="h-4 w-4" />
+                </Button>
+            ),
+            cell: ({ row }) => (
+                <span className=" bg-yellow-200 rounded-lg px-4 py-1">
+                    {row.original.kategori.name}
+                </span>
+            ),
             sortingFn: "text",
         },
         {
@@ -142,57 +138,50 @@ const Index = ({ kategori, produk }) => {
         {
             id: "deskripsion",
             accessorKey: "deskripsion",
-            header: ({ column }) => {
-                return (
-                    <Button
-                        variant="tableHeader"
-                        onClick={() =>
-                            column.toggleSorting(column.getIsSorted() === "asc")
-                        }
-                    >
-                        Deskripsi
-                        <ArrowUpDown className="h-4 w-4" />
-                    </Button>
-                );
-            },
+            header: ({ column }) => (
+                <Button
+                    variant="tableHeader"
+                    onClick={() =>
+                        column.toggleSorting(column.getIsSorted() === "asc")
+                    }
+                >
+                    Deskripsi
+                    <ArrowUpDown className="h-4 w-4" />
+                </Button>
+            ),
             sortingFn: "text",
         },
         {
             accessorKey: "image",
             header: "Gambar",
-            cell: ({ row }) => {
-                return (
-                    <img
-                        className="size-32 md:size-20 rounded-lg object-contain"
-                        src={
-                            window.location.origin +
-                            "/storage/" +
-                            row.getValue("image")
-                        }
-                        alt=""
-                    />
-                );
-            },
+            cell: ({ row }) => (
+                <img
+                    className="size-32 md:size-20 rounded-lg object-contain"
+                    src={
+                        window.location.origin +
+                        "/storage/" +
+                        row.getValue("image")
+                    }
+                    alt=""
+                />
+            ),
         },
         {
             accessorKey: "price",
-            header: "Harga",
-            header: ({ column }) => {
-                return (
-                    <Button
-                        variant="tableHeader"
-                        onClick={() =>
-                            column.toggleSorting(column.getIsSorted() === "asc")
-                        }
-                    >
-                        Deskripsi
-                        <ArrowUpDown className="h-4 w-4" />
-                    </Button>
-                );
-            },
-            cell: ({ row }) => {
-                return <FormatRupiah value={row.getValue("price")} />;
-            },
+            header: ({ column }) => (
+                <Button
+                    variant="tableHeader"
+                    onClick={() =>
+                        column.toggleSorting(column.getIsSorted() === "asc")
+                    }
+                >
+                    Harga
+                    <ArrowUpDown className="h-4 w-4" />
+                </Button>
+            ),
+            cell: ({ row }) => (
+                <FormatRupiah value={row.getValue("price")} />
+            ),
         },
         {
             accessorKey: "created_at",
@@ -222,13 +211,10 @@ const Index = ({ kategori, produk }) => {
                         <Link
                             preserveScroll
                             preserveState
-                            className="text-xs font-semibold bg-green-100 border border-green-500 px-4 py-2 rounded-md text-green-500  hover:bg-green-200 hover:text-gray-white hover:border-gray-400"
+                            className="text-xs font-semibold bg-green-100 border border-green-500 px-4 py-2 rounded-md text-green-500 hover:bg-green-200 hover:text-gray-white hover:border-gray-400"
                             type="button"
                             method="get"
-                            href={route(
-                                "admin.produk.restore",
-                                produk.id
-                            )}
+                            href={route("admin.produk.restore", produk.id)}
                         >
                             Restore
                         </Link>
